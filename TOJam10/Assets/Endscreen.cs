b9edcbcae54488b89825e633bc65e180e7c9c757
Alexander Biggs <alexanderkbiggs@gmail.com>
2015-05-03 18:47:04 -0400

    end screen

---
+++ TOJam10/Assets/Endscreen.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Endscreen : MonoBehaviour
+{
+    private bool canExit;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    this.canExit = false;
+	    Timer.Register(2f, () =>
+	    {
+	        this.canExit = true;
+	    });
+	}
+
+    void Update()
+    {
+        Timer.UpdateAllRegisteredTimers();
+
+        if (this.canExit && Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("???");
+            Application.LoadLevel(0);
+        }
+    }
+}
